ef97862c6cf6960378c8dde5b4472d492be3a910
xFijo <75216577+xFijo@users.noreply.github.com>
2021-12-19 13:42:03 +0000

    Update calc.py

--- calc.py
+++ calc.py
@@ -5,22 +5,22 @@ import time
 multiply = '*'
 plus = '+'
 minus = '-'
-
+pass
 def clear():
     os.system('cls')
     print(crayons.green('Python Calculator By xFijo'))
     os.system("Title Python Calculator.")
-
+pass
 clear()
-
+pass
 print(crayons.cyan('\nFirst Number : '))
 number1 = int(input())
 print(crayons.green(f'Number [{number1}] Chosen.'))
-
+pass
 print(crayons.cyan('\nSecond Number : '))
 number2 = int(input())
 print(crayons.green(f'Number [{number2}] Chosen.'))
-
+pass
 def meth():
    method = input(crayons.cyan('\nEnter Method : [*] [-] [+] : '))
    print(crayons.green(f'Method [{method}] Chosen.'))
@@ -33,5 +33,5 @@ def meth():
        print(crayons.green(f'{number1} - {number2} = {number1 - number2}'))
    else:
        print(crayons.red('Invalid Method.'))
-
+pass
 meth()
